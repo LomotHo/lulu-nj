f1374dae7dff1890e341a3324666ddc68afd6758
lomot <lomot@qq.com>
2020-06-11 14:27:38 +0800

    add comments

--- controller/comments-page.js
+++ controller/comments-page.js
@@ -2,9 +2,11 @@ const action = require("../dao");
 
 module.exports = {
   'GET /comments': async (ctx, next) => {
+    let comments = await action.comments.getAll();
     ctx.render('comments.njk', {
       "ui": {
         title: "留言板",
+        comments: comments
       }
     });
   }
